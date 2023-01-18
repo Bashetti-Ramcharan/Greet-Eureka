1e92cf8260f96153e950e4f85568ba2387faf920
RamCharan <bashettiramcharan1997uppi@gmail.com>
2023-01-18 19:16:27 +0530

    Adding the project for the first time

---
+++ src/test/java/com/example/greeteurekaclient/GreetEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greeteurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
